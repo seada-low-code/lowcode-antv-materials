ed075be966079a556d2d1fd3de964d5797665349
haloworld007 <891567977@qq.com>
2023-03-27 16:22:48 +0800

    feat: 更新line坐标轴配置

--- lowcode/line/meta.ts
+++ lowcode/line/meta.ts
@@ -1,5 +1,101 @@
-import { IPublicTypeComponentMetadata, IPublicTypeSnippet } from '@alilc/lowcode-types';
+import {
+  IPublicTypeComponentMetadata,
+  IPublicTypeFieldConfig,
+  IPublicTypeSnippet,
+} from '@alilc/lowcode-types';
 import { containerMeta, dataSourceMeta } from '../common';
+
+const getAxisMetaData = (axisName: string): IPublicTypeFieldConfig => {
+  return {
+    title: {
+      label: axisName,
+    },
+    display: 'accordion',
+    type: 'group',
+    items: [
+      {
+        name: `${axisName}.top`,
+        title: {
+          label: 'top',
+          tip: '是否渲染在画布顶层',
+        },
+        setter: {
+          componentName: 'BoolSetter',
+        },
+      },
+      {
+        name: `${axisName}.position`,
+        title: {
+          label: 'position',
+          tip: '坐标轴的位置',
+        },
+        setter: {
+          componentName: 'RadioGroupSetter',
+          props: {
+            options: [
+              {
+                label: 'top',
+                value: 'top',
+              },
+              {
+                label: 'bottom',
+                value: 'bottom',
+              },
+              {
+                label: 'left',
+                value: 'left',
+              },
+              {
+                label: 'right',
+                value: 'right',
+              },
+            ],
+          },
+        },
+      },
+      {
+        name: `${axisName}.min`,
+        title: {
+          label: 'min',
+          tip: '坐标轴最小值',
+        },
+        setter: {
+          componentName: 'NumberSetter',
+          props: {
+            min: 0,
+          },
+          initialValue: 0,
+        },
+      },
+      {
+        name: `${axisName}.max`,
+        title: {
+          label: 'max',
+          tip: '坐标轴最大值',
+        },
+        setter: {
+          componentName: 'NumberSetter',
+          props: {
+            min: 0,
+          },
+        },
+      },
+      {
+        name: `${axisName}.tickCount`,
+        title: {
+          label: 'tickCount',
+          tip: '期望的坐标轴刻度数量',
+        },
+        setter: {
+          componentName: 'NumberSetter',
+          props: {
+            min: 0,
+          },
+        },
+      },
+    ],
+  };
+};
 
 const LineMeta: IPublicTypeComponentMetadata = {
   componentName: 'Line',
@@ -149,6 +245,14 @@ const LineMeta: IPublicTypeComponentMetadata = {
         ],
       },
       { ...containerMeta },
+      {
+        title: {
+          label: '图表组件',
+        },
+        display: 'accordion',
+        type: 'group',
+        items: [{ ...getAxisMetaData('xAxis') }, { ...getAxisMetaData('yAxis') }],
+      },
     ],
     supports: {},
     component: {},
