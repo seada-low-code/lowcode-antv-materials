87d4341130b7f476aa8647c22901be326ab84b93
haloworld007 <891567977@qq.com>
2023-06-27 19:04:34 +0800

    feat: add area meta

---
+++ lowcode/area/meta.ts
@@ -0,0 +1,76 @@
+import { IPublicTypeComponentMetadata } from '@alilc/lowcode-types';
+import { axisMeta, containerMeta, dataSourceMeta, labelMeta, legendMeta } from '../common';
+import { snippets } from './snippets';
+
+const AreaMeta: IPublicTypeComponentMetadata = {
+  componentName: 'Bar',
+  title: 'Bar',
+  docUrl: '',
+  screenshot: '',
+  devMode: 'proCode',
+  group: 'AntV',
+  category: '条形图',
+  npm: {
+    package: '@seada/antv-materials',
+    version: '0.1.0',
+    exportName: 'Bar',
+    main: '',
+    destructuring: true,
+    subName: '',
+  },
+  configure: {
+    props: [
+      { ...dataSourceMeta },
+      {
+        title: {
+          label: '数据映射',
+        },
+        display: 'accordion',
+        type: 'group',
+        items: [
+          {
+            name: 'xField',
+            title: {
+              label: 'xField',
+              tip: 'x方向对应的字段名',
+            },
+            setter: 'StringSetter',
+            defaultValue: 'x',
+          },
+          {
+            name: 'yField',
+            title: {
+              label: 'yField',
+              tip: 'y方向对应的字段名',
+            },
+            setter: 'StringSetter',
+            defaultValue: 'y',
+          },
+          {
+            name: 'seriesField',
+            title: {
+              label: 'seriesField',
+              tip: '拆分字段，在分组条形图下同 groupField、colorField，在堆积条形图下同 stackField、colorField',
+            },
+            setter: 'StringSetter',
+          },
+        ],
+      },
+      { ...containerMeta },
+      { ...axisMeta },
+      {
+        ...labelMeta,
+      },
+      {
+        ...legendMeta,
+      },
+    ],
+    supports: { style: true },
+    component: {},
+  },
+};
+
+export default {
+  ...AreaMeta,
+  snippets,
+};
